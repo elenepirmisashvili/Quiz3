4e683070ccd9bb9b13b7ad6b1d5e4e4ca9a78e0b
elenepirmisashvili <e_pirmisashvili@cu.edu.ge>
2021-11-21 22:48:26 +0400

    fix

---
+++ src/test/java/SelenideBasics2Test.java
@@ -0,0 +1,33 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.AssertionMode.SOFT;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideBasics2Test {
+
+
+    @Test
+    public void booksTask() {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/books");
+        driver.manage().window().maximize();
+        $$(".rt-tr-group").
+                filterBy(Condition.and("Name", Condition.text("O'Reilly Media"), Condition.text("Javascript")));
+    }
+    @Test
+    public void errorOfSize() {
+
+        Configuration.assertionMode = SOFT;
+        Configuration.browser="Chrome";
+        open("https://demoqa.com/books");
+        System.out.println( $(".rt-tbody").findAll(".rt-tr-group").filterBy(text("O'Reilly Media")).filterBy(text("Javascript")).shouldHave(size(10)));
+    }
+}
